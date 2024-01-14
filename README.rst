3c5b964fd274efc29cd8ccbe7244e881a412dfee
Lura Skye <l@veriny.tf>
2024-01-14 04:00:49 +0000

    start docs

---
+++ README.rst
@@ -0,0 +1,7 @@
+Kamuidrome
+----------
+
+Kamuidrome is a Minecraft modpack creation helper that integrates directly with Prism Launcher.
+
+See the docs at https://kamuidrome.readthedocs.io/ for more information.
+
---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from importlib.metadata import version as get_version
+
+project = "kamuidrome"
+copyright = "2024, Lura Skye"
+author = "Lura Skye"
+release = get_version("kamuidrome")
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinxcontrib.jquery",
+    "sphinx_inline_tabs",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css"]
+
+html_theme_options = {
+    "collapse_navigation": False,
+    "style_external_links": True,
+}
