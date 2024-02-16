bb6f1747d885a36e59c392fc48068d5a7d8fbb9e
Lura Skye <l@veriny.tf>
2024-02-16 13:39:17 +0000

    deal with modrinth trolling us for no fucking reason

--- src/kamuidrome/modrinth/mrpack.py
+++ src/kamuidrome/modrinth/mrpack.py
@@ -123,7 +123,7 @@ def create_mrpack(
             if server_only and mod.client_side_only:
                 print(f"[yellow]not exporting {mod.name}[/yellow]")
                 continue
-            
+
             print(f"[yellow] definitely exporting {mod.name} ({mod.version_id})")
             version_ids.append(mod.version_id)
 
